19a2aa84e95b23d7088473de7efcd9c8189cf850
Agustina Cateura <164643096+aguscateura@users.noreply.github.com>
2025-07-16 13:10:44 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,17 @@
+// Script barra de navegación
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const cerrar = document.getElementById('close');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active')
+    })
+}
+
+if (cerrar) {
+    cerrar.addEventListener('click', (e) => {
+        e.preventDefault();
+        nav.classList.remove('active')
+    })
+}
